3e8fc3a3b0623295469e524dcc9ad60c63a64204
Hannah <hannahwoodward@hotmail.com>
2022-02-16 11:59:23 +0800

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
 require_relative "./vehicle.rb"
+# require_relative should be used for referring to files within your directory
+# require_relative is a subset of require and is a convenient method to use when you are referring to a file that is relative to the current file
+
+class Car < Vehicle
+    def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+  end
